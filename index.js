bd6c13dd0f2b258e57e827bb5a0c3f836d38b156
muskanBP <>
2024-04-12 01:33:39 +0500

    push my todos to githubt

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestions = await inquirer.prompt([
+        {
+            name: "questionFirst",
+            type: "input",
+            message: "would you like to add in you todos?"
+        },
+        {
+            name: "questionSecond",
+            type: "confirm",
+            message: "would you like to add more in your todos?",
+            default: "true"
+        }
+    ]);
+    todos.push(todoQuestions.questionFirst);
+    console.log(todos);
+    condition = todoQuestions.questionSecond;
+}
